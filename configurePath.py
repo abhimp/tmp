7a51750c454629bc40d0d34f60125ba541f63b34
Abhijit Mondal <abhijit.manpur@gmail.com>
2017-12-31 00:03:02 +0530

    update

--- configurePath.py
+++ configurePath.py
@@ -79,7 +79,7 @@ def loadInfo():
 
     localrout = []
     for iface in intfs:
-        localrout += [getDefaultRoute(iface[0], iface[1], iface[1])]
+        localrout += [getDefaultRoute(iface[0], iface[1], iface[2])]
         removeRoutingTable(iface[2])
     print intfs
     print routes
