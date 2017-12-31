9b892cfded6962cafc084b799ab8fa9558defe75
Abhijit Mondal <abhijit.manpur@gmail.com>
2017-12-31 10:25:22 +0530

    update

--- configurePath.py
+++ configurePath.py
@@ -46,12 +46,13 @@ def getNextHopIp(edges, s, r):
     ip2Net = {x[0] if not x[1].startswith("s") else x[1]:x[2] for x in edges if x[0] == r or x[1] == r}
     return ip2Net.get(s, None)
 
-def adjustRouting(routs):
+def adjustRouting(routs, edges):
     newRouts = []
     for x in routs:
         net = netaddr.IPNetwork(x[0])
         x[0] = "%s/%s"%(net.network, net.prefixlen)
-        newRouts += [x]
+        y = [x[0], getNextHopIp(edges, x[1], x[2])]
+        newRouts += [y]
     return newRouts
         
 
@@ -70,7 +71,7 @@ def setRoutingTable(net2Ip, routes):
         ip = net2Ip[x[1]]
         ifc = ips.get(ip[0], "unknown")
         #network = netaddr.IPNetwork(x[0])
-        route = "route add -net %s/%s gw %s dev %s"%(x[0], getNextHopIp(edges, x[1], x[2]), ifc)
+        route = "route add -net %s gw %s dev %s"%(x[0], x[1], ifc)
         print route
         routeEntries += [route]
         intfs.add((ip[0], ip[1], ifc))
@@ -97,7 +98,7 @@ def loadInfo():
     
     net2Ip = {x[0] if not x[1].startswith("s") else x[1]:(x[2], x[3]) for x in edges if x[0] == host or x[1] == host}
     #print net2Ip
-    route = adjustRouting(info['rout'][host])
+    route = adjustRouting(info['rout'][host], edges)
     print net2Ip
 
     setRoutingTable(net2Ip, route)
