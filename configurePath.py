caa3960d2c602428bfd54229a7f1c5bc790e9726
Abhijit Mondal <abhijit.manpur@gmail.com>
2017-12-31 12:25:13 +0530

    update

--- configurePath.py
+++ configurePath.py
@@ -46,13 +46,20 @@ def getNextHopIp(edges, s, r):
     ip2Net = {x[0] if not x[1].startswith("s") else x[1]:x[2] for x in edges if x[0] == r or x[1] == r}
     return ip2Net.get(s, None)
 
-def adjustRouting(routs, edges):
+def adjustRouting(routs, host, edges):
+    if not host.startswith("h"):
+        routs = routs[host]
+    else:
+        pseudoRouts = [y+[x.split("x")[1]] for x in routs if x.startswith(host+"x") for y in routs[x]]
+        print pseudoRouts
+        routs = pseudoRouts
+        
     newRouts = []
     for x in routs:
         net = netaddr.IPNetwork(x[0])
         x[0] = "%s/%s"%(net.network, net.prefixlen)
-        y = [x[0], x[1], getNextHopIp(edges, x[1], x[2])]
-        newRouts += [y]
+        x[2] = getNextHopIp(edges, x[1], x[2])
+        newRouts += [x]
     return newRouts
         
 
@@ -63,6 +70,7 @@ def setRoutingTable(net2Ip, routes):
     #create local routes to 
 
     intfs = set()
+    ipRuleRoute = []
     for x in routes:
         #print x[0], net2Ip[x[1]], getNextHopIp(edges, x[1], x[2])
         if net2Ip[x[1]][0] not in ips:
@@ -70,17 +78,20 @@ def setRoutingTable(net2Ip, routes):
         #print x[0], ips.get(net2Ip[x[1]][0], "unknown"), getNextHopIp(edges, x[1], x[2])
         ip = net2Ip[x[1]]
         ifc = ips.get(ip[0], "unknown")
+        localNet = netaddr.IPNetwork(ip[0], ip[1])
         #network = netaddr.IPNetwork(x[0])
-        route = "route add -net %s gw %s dev %s"%(x[0], x[2], ifc)
-        print route
-        routeEntries += [route]
+        routeEntries += ["route add -net %s gw %s dev %s"%(x[0], x[2], ifc)]
+        if len(x) > 3 and x[2] == "0.0.0.0/0":
+            ipRuleRoute += [ "ip rule add from {ip} table {table}".format(ip=net2Ip[x[1]][0], table=x[3]) ]
+            ipRuleRoute += [ "ip route add {network} dev {ifc} scope link table {table}".format(network=localNet.network+"/"+localNet.prefixlen, ifc=ifc, table=x[3])
+            routeEntries += ["ip route add default via %s dev %s table %s"%(x[2], ifc, x[3])]
         intfs.add((ip[0], ip[1], ifc))
 
     localrout = []
     for iface in intfs:
         #print "iface", iface
-        localrout += [getDefaultRoute(iface[0], iface[1], iface[2])]
         removeRoutingTable(iface[2])
+        localrout += [getDefaultRoute(iface[0], iface[1], iface[2])]
     #print intfs
     #print routes
 
@@ -98,9 +109,12 @@ def loadInfo():
     
     net2Ip = {x[0] if not x[1].startswith("s") else x[1]:(x[2], x[3]) for x in edges if x[0] == host or x[1] == host}
     #print net2Ip
-    route = adjustRouting(info['rout'][host], edges)
+    route = adjustRouting(info['rout'], host, edges)
     print net2Ip
 
+
+    if "VHOST" in os.environ and len(os.environ["VHOST"]) != 0:
+        return
     setRoutingTable(net2Ip, route)
 
 
